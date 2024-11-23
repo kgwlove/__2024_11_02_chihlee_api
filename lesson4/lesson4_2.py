e83487a28cfac961f821a6c8aaab9414b04e8c01
kgwlove <kgwlove@gmail.com>
2024-11-23 03:38:01 +0000

    modify

--- lesson4/lesson4_2.py
+++ lesson4/lesson4_2.py
@@ -3,30 +3,13 @@
 #檢查格式是否錯誤
 #計算出BMI,print
 
-def bmiequ(weight, height)->tuple[str,float]:
-    '''
-    docstring
-    parameter:
-     bmi:bmi值可以整數或浮點數
-    Return:str
-         會傳出bmi關聯的體重狀態
-    '''
-    #BMI僅留小數點兩位
-    bmi=round(weight/((height/100)**2), 2)
-    if bmi<18.5:
-        Wstate='體重過輕'
-    elif bmi<24:
-        Wstate='正常範圍'
-    elif bmi<27:
-        Wstate='異常範圍:過重'
-    elif bmi<30:
-        Wstate='異常範圍:輕度肥胖'
-    elif bmi<35:
-        Wstate='異常範圍:中度肥胖'      
-    else:
-        Wstate='異常範圍:重度肥胖'
-    return Wstate, bmi
-    
+
+
+
+#py專案主程式
+#使用main()
+
+import tools
 def main():
     try:
         height = float(input('輸入身高cm:'))
@@ -38,7 +21,7 @@ def main():
         except Exception:
             print('輸入格式錯誤')
         else:
-            Wstate_n,bmi_n= bmiequ(weight,height)
+            Wstate_n,bmi_n= tools.bmiequ(weight,height)
 
     print(f'您的身高={str(height)}cm,您的體重={weight}kg')            
     print(f'BMI={bmi_n},屬於{Wstate_n}') 
---
+++ lesson4/tools.py
@@ -0,0 +1,28 @@
+PI=3.1415926
+
+class Person:
+    pass
+
+def bmiequ(weight, height)->tuple[str,float]:
+    '''
+    docstring
+    parameter:
+     bmi:bmi值可以整數或浮點數
+    Return:str
+         會傳出bmi關聯的體重狀態
+    '''
+    #BMI僅留小數點兩位
+    bmi=round(weight/((height/100)**2), 2)
+    if bmi<18.5:
+        Wstate='體重過輕'
+    elif bmi<24:
+        Wstate='正常範圍'
+    elif bmi<27:
+        Wstate='異常範圍:過重'
+    elif bmi<30:
+        Wstate='異常範圍:輕度肥胖'
+    elif bmi<35:
+        Wstate='異常範圍:中度肥胖'      
+    else:
+        Wstate='異常範圍:重度肥胖'
+    return Wstate, bmi
