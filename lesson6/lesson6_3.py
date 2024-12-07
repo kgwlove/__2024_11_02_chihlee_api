abe60907ce87b0c6abb44a2f259804e7a45dac50
kgwlove <gbmder@gmail.com>
2024-12-07 06:38:14 +0000

    modify

--- lesson6/lesson6_3.py
+++ lesson6/lesson6_3.py
@@ -3,15 +3,10 @@ from pprint import pprint #用來看dictionary資料方便看
 import tools
 
 def main():
-    data:list[dict] = tools.get_aqi(excel_name='aqi.xlsx')
-    sitenames:list=[]
-    for item in data:
-        sitenames.append(item['sitename'])
-    sitenames = list(set(sitenames)) #將sitenames變set,再變成list。沒有重複的項目了
-            
+    sitenames:list[str] = tools.get_sitenames(excel_name='aqi.xlsx')
     print (len(sitenames))
     pprint (sitenames)
-
+    
 #以下為執行的第一行敘述
 if __name__=='__main__':
     main()
