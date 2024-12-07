4fcb425610950088529da2ddd511ad3ec2c2f31f
kgwlove <gbmder@gmail.com>
2024-12-07 07:43:05 +0000

    modify

---
+++ lesson6/lesson6_4.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import tools
+
+sitenames:list[str] = tools.get_sitenames(excel_name='aqi.xlsx')
+#object notation
+add_selectbox = st.sidebar.selectbox(  #提供站名選擇
+    "請選擇站點名稱",
+    sitenames
+)
+
+
+#執行方法:終端機輸入 $streamlit run lesson6_4.py
