a961e1ea120872afbcce8525d3b73e1cc974205f
kgwlove <gbmder@gmail.com>
2024-12-07 07:05:36 +0000

    modify

--- lesson6/lesson6_3.py
+++ lesson6/lesson6_3.py
@@ -1,15 +1,20 @@
-
 from pprint import pprint #用來看dictionary資料方便看
+import openpyxl
+from openpyxl import Workbook, worksheet
 import tools
 
 def main():
     sitenames:list[str] = tools.get_sitenames(excel_name='aqi.xlsx')
-    print (len(sitenames))
-    pprint (sitenames)
+    wb:Workbook = openpyxl.Workbook()
+    sheet:worksheet = wb.active
+    sheet.title = "站點名稱"
+    for idy ,name in enumerate(sitenames):
+        print (name)
+        sheet.cell(column=1, row=idy+1, value=name)
+    wb.save('老闆要的資訊.xlsx')    
+
     
 #以下為執行的第一行敘述
 if __name__=='__main__':
     main()
-
-
-        
+       
