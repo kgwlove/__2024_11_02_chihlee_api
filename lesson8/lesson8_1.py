c808f3d32fb60e9a6d322b3b28e6a9ad5c6802f0
kgwlove <gbmder@gmail.com>
2024-12-21 02:30:44 +0000

    modify

---
+++ lesson8/lesson8_1.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+#手動建立counter_key,
+if "counter" not in st.session_state:
+    #st.session_state['counter'] = 0
+    st.session_state.counter = 1
+
+def increment_counter(*args):
+   #st.session_state.counter += 1
+   st.session_state.counter += args[0]
+   st.session_state.counter -= args[1]
+
+st.header(f"這個網頁已經執行{st.session_state.counter} 次")
+st.button("再執行一次", key="reset",help='按下要加1', on_click=increment_counter,args=(5,3)) #自動建立reset_key
